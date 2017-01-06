292bf1588c7d16a8deab165ce56adc393b503c8c
Etienne Carriere <etienne.carriere@linaro.org>
2017-01-06 17:33:14 +0100

    Make sure "make clean" removes everything in TA devkit.
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

--- ta/mk/ta_dev_kit.mk
+++ ta/mk/ta_dev_kit.mk
@@ -5,6 +5,31 @@ ta-dev-kit-dir := $(patsubst %/,%,$(abspath $(dir $(lastword $(MAKEFILE_LIST))).
 .PHONY: all
 all:
 
+define _enum-parent-dirs
+$(if $(1),$(1) $(if $(filter / ./,$(dir $(1))),,$(call enum-parent-dirs,$(dir $(1)))),)
+endef
+
+define enum-parent-dirs
+$(call _enum-parent-dirs,$(patsubst %/,%,$(1)))
+endef
+
+define reverse
+$(if $(1),$(call reverse,$(wordlist 2,$(words $(1)),$(1)))) $(firstword $(1))
+endef
+
+# Returns the list of all existing output directories up to $(O) including all
+# intermediate levels, in depth first order so that rmdir can process them in
+# order. May return an empty string.
+# Example: if cleandirs is "foo/a foo/b/c/d" and O=foo, this will return
+# "foo/b/c/d foo/b/c foo/b foo/a" (assuming all exist).
+define cleandirs-for-rmdir
+$(wildcard $(addprefix $(O)/,
+                       $(call reverse,$(sort
+                           $(foreach dir,$(patsubst $(O)/%,%,$(cleandirs)),
+                                         $(call enum-parent-dirs,$(dir)))))))
+endef
+
+
 include $(ta-dev-kit-dir)/mk/conf.mk
 
 binary := $(BINARY)
@@ -72,7 +97,9 @@ libdeps += $(ta-dev-kit-dir)/lib/libpng.a
 clean:
 	@$(cmd-echo-silent) '  CLEAN   .'
 	${q}rm -f $(cleanfiles)
-
+	${q}dirs="$(call cleandirs-for-rmdir)"; if [ "$$dirs" ]; then rmdir $$dirs; fi
+	@$(cmd-echo-silent) '  CLEAN   $(O)'
+	${q}if [ -d "$(O)" ]; then rmdir --ignore-fail-on-non-empty $(O); fi
 
 subdirs = .
 include  $(ta-dev-kit-dir)/mk/subdir.mk
